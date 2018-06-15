c0b8da053e980135f377e21a12e84b1068b445f0
biletboh <biletskyboh@gmail.com>
2018-06-15 18:40:39 +0300

    added fontawesome, fixed contacts page, added scripts for active links in navbar

--- shkola/urls.py
+++ shkola/urls.py
@@ -6,11 +6,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from blog.views import PostList
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
     path('upload/', include('django_file_form.urls')),
 ] + i18n_patterns(
+        path('', PostList.as_view()),
         path('', include('information.urls')),
         path('blog/', include('blog.urls')),
         path('teachers/', include('theteachers.urls')),
